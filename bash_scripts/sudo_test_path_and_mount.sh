0b8c26752e4f6c5579a7243dd29b7c866bb109ec
LateJune <johnsoler24@gmail.com>
2022-06-23 01:24:57 -0400

    change spacing, change & add console logging

--- bash_scripts/sudo_test_path_and_mount.sh
+++ bash_scripts/sudo_test_path_and_mount.sh
@@ -6,51 +6,53 @@ while
 	
 	if [[ $(ls /tmp | grep .b64| wc --words) > 0 ]]
 		then
-			printf "[+] Grep found $(ls /tmp | grep .b64| wc --words) files in /tmp. Moving to $final_path\n"
+			printf "[+] $(ls /tmp | grep .b64| wc --words) New .b64 files in /tmp. Moving to $final_path\n"
 			ls /tmp | grep .b64 | sudo xargs -r -I{} mv /tmp/{} $final_path
 		else
-			printf "[+] Moving to mnt\n"
+			printf "[x] No new files in /tmp\n"
 	fi
 do
-if [[ -f "$mount_path/flag" && $(ls $mount_path | wc --words) > 0 ]]
-	then
-		printf "[+] Flag is present in the path and number of words are greater than zero, attempting to unmount the share\n"
-		echo "$su_pass" | sudo umount $mount_path -l
+	if [[ -f "$mount_path/flag" && $(ls $mount_path | wc --words) > 0 ]]
+		then
+			printf "[+] Flag is present in the path and number of words are greater than zero, attempting to unmount the share\n"
+			echo "$su_pass" | sudo umount $mount_path -l
 
-		if [[ $(ls $mount_path | wc --words) = 0 ]]
-			then
-				printf "[+] unmount successful! $mount_path has $(ls $mount_path | wc --words) words present\n"
+			if [[ $(ls $mount_path | wc --words) = 0 ]]
+				then
+					printf "[+] unmount successful! $mount_path has $(ls $mount_path | wc --words) words present\n"
 
-			else
-				printf "[x] unmount was unsuccessful" 
-		fi
+				else
+					printf "[x] unmount was unsuccessful" 
+			fi
 
-		printf "[-] Sleeping for 5 seconds\n"
-		sleep 5
+			printf "[-] Sleeping for 5 seconds\n"
+			sleep 5
 
-else
-	printf "[+] Flag is not present and number of words are more than zero, running the mount portion\n"
-	while
-		# Check if anything is mounted
-		# if mount was successful, then the number of words returned will not be zero
-		(( $(ls $mount_path | wc --words) <= 0 ))
-	do 
+	else
+		printf "[+] Flag is not present and number of words are more than zero, running the mount portion\n"
+		while
+			# Check if anything is mounted
+			# if mount was successful, then the number of words returned will not be zero
+			(( $(ls $mount_path | wc --words) <= 0 ))
+		do 
 
-		printf "[+] Attempting to mount\n"
-		echo "$su_pass" | sudo -S mount -v -t cifs -o username=$user,password=$share_pass,port=$port //$share_ip/$mount_folder $mount_path 
-		
-		if [[ $(ls $mount_path | wc --words) > 0 ]]
-			then
-				printf "[+] Mount was successful! $mount_path has $(ls $mount_path | wc --words) words present\n"
-			else
-				printf "[x] Mount was unsuccessful" 
-		fi 
-		printf "[-] Sleeping for 5 seconds\n"
-		sleep 5
-	done
-fi 
-printf "[+] Exiting execution\n"
-printf "[-] Sleeping for 5 seconds\n"
-sleep 5
+			printf "[+] Attempting to mount\n"
+			echo "$su_pass" | sudo -S mount -v -t cifs -o username=$user,password=$share_pass,port=$port //$share_ip/$mount_folder $mount_path 
+
+			if [[ $(ls $mount_path | wc --words) > 0 ]]
+				then
+					printf "[+] Mount was successful! $mount_path has $(ls $mount_path | wc --words) words present\n"
+				else
+					printf "[x] Mount was unsuccessful" 
+			fi 
+			printf "[-] Sleeping for 5 seconds\n"
+			sleep 5
+		done
+	fi
+
+	printf "### Reached End of Main Loop ###\n"
+	printf "[-] Sleeping for 10 seconds\n\n"
+	sleep 10
 done
-exit main loop
+
+printf "[+] Exiting execution\n"
