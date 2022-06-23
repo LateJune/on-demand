5d30f3d8e0d7a1e6b0605b4fa824acf8d2255c13
LateJune <johnsoler24@gmail.com>
2022-06-23 01:13:28 -0400

    add helper loop

--- bash_scripts/sudo_test_path_and_mount.sh
+++ bash_scripts/sudo_test_path_and_mount.sh
@@ -2,6 +2,16 @@
 
 source .env
 
+while
+	
+	if [[ $(ls /tmp | grep .b64| wc --words) > 0 ]]
+		then
+			printf "[+] Grep found $(ls /tmp | grep .b64| wc --words) files in /tmp. Moving to $final_path\n"
+			ls /tmp | grep .b64 | sudo xargs -r -I{} mv /tmp/{} $final_path
+		else
+			printf "[+] Moving to mnt\n"
+	fi
+do
 if [[ -f "$mount_path/flag" && $(ls $mount_path | wc --words) > 0 ]]
 	then
 		printf "[+] Flag is present in the path and number of words are greater than zero, attempting to unmount the share\n"
@@ -18,13 +28,13 @@ if [[ -f "$mount_path/flag" && $(ls $mount_path | wc --words) > 0 ]]
 		printf "[-] Sleeping for 5 seconds\n"
 		sleep 5
 
-	else
-		printf "[+] Flag is not present and number of words are more than zero, running the mount portion\n"
-		while
+else
+	printf "[+] Flag is not present and number of words are more than zero, running the mount portion\n"
+	while
 		# Check if anything is mounted
 		# if mount was successful, then the number of words returned will not be zero
 		(( $(ls $mount_path | wc --words) <= 0 ))
-		do 
+	do 
 
 		printf "[+] Attempting to mount\n"
 		echo "$su_pass" | sudo -S mount -v -t cifs -o username=$user,password=$share_pass,port=$port //$share_ip/$mount_folder $mount_path 
@@ -39,5 +49,8 @@ if [[ -f "$mount_path/flag" && $(ls $mount_path | wc --words) > 0 ]]
 		sleep 5
 	done
 fi 
-
 printf "[+] Exiting execution\n"
+printf "[-] Sleeping for 5 seconds\n"
+sleep 5
+done
+exit main loop
