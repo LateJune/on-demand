f80f4d86d5c750168fb51565664544f5cb73992b
LateJune <johnsoler24@gmail.com>
2022-09-15 21:50:45 -0400

    Added most of logic to proctor a session, fixed typo in function name

--- bash_scripts/sandbox_proctor.sh
+++ bash_scripts/sandbox_proctor.sh
@@ -36,26 +36,18 @@ function resetVMSession {
     # Remove active session flag
     rm /tmp/flag 
 
-    # if is mounted is true
-    if [[ is_file_share_mounted -eq 0 ]] 
-    then        
-        unmountShare
+    if [[ is_file_share_mounted -eq 1 ]] 
+    then         
         powerOffAndRestoreVM
-    # Otherwise power off and restore
-    else        
+    else 
+        unmountShare       
         powerOffAndRestoreVM
     fi
 }
 
-function writeBase64FilestToShare {
-    if [[ is_b64_file_present -gt 0 && is_file_share_mounted -eq 0 ]]
-    then
-        printf "[+] $(ls /tmp | grep .b64| wc --words) New .b64 files in /tmp. Moving to $final_path\n"
-        ls /tmp | grep .b64 | sudo xargs -r -I{} mv /tmp/{} $final_path
-    else
-        printf "[x] No new files in /tmp\n"
-        continue
-    fi
+function writeBase64FilesToShare {
+    printf "[+] $(ls /tmp | grep .b64| wc --words) New .b64 files in /tmp. Moving to $final_path\n"
+    ls /tmp | grep .b64 | sudo xargs -r -I{} mv /tmp/{} $final_path
 }
 
 function sessionTimeout {
@@ -68,20 +60,70 @@ function sessionTimeout {
     fi
 }
 
+
 while
     # 0 == True
     # 1 == False
-    is_user_sshed=$( [[ $(ss | grep ssh | wc --lines) -eq 0 ]]; printf $?)    
+    is_user_sshed=$( [[ $(ss | grep ssh | wc --lines) -ne 0 ]]; printf $?)    
     is_file_share_mounted=$( [[ $(ss  |  grep 127.0.0.1:445 | wc --lines) -ne 0 ]]; printf $?)
     is_b64_file_present=$( [[ $(ls /tmp | grep .b64| wc --words) -gt 0 ]]; printf $?)
     is_unmount_flag_present=$( test -f "$mount_path/flag"; printf $?)
-    is_user_active_session=$( test -f "/tmp/flag"; printf $?)
+    is_active_session=$( test -f "/tmp/flag"; printf $?)
+
+    printf "\nis_user_sshed: $is_user_sshed\n"
+    printf "is_file_share_mounted: $is_file_share_mounted\n"
+    printf "is_b64_file_present: $is_b64_file_present\n"
+    printf "is_unmount_flag_present: $is_unmount_flag_present\n"
+    printf "is_active_session: $is_active_session\n"
 
 do
-    if [[ ssh_value -eq 0 ]] 
-    then
-        continue
+    if [[ is_user_sshed -eq 0 ]] 
+        then
+        # If sshed, not active session, and file share is not mounted
+        if [[ $is_active_session -eq 1 && $is_file_share_mounted -eq 1 ]]
+            then
+            startVMAndMount
+            if [[ is_idle -eq 1 ]]
+                then
+                is_idle=0
+            fi  
+
+        # If is active session and file share is monuted
+        elif [[ $is_active_session -eq 0 && $is_file_share_mounted -eq 0 ]]
+            then
+            # If is active session, .b64 files are present, and no unmount flag present
+            if [[ $is_active_session -eq 0 && $is_b64_file_present -eq 0 && $is_unmount_flag_present -eq 1 ]]
+                then
+                writeBase64FilesToShare
+            # If unmount flag is present in file share 
+            elif [[ $is_unmount_flag_present -eq 0 ]]
+                then 
+                unmountShare
+
+            else
+                #printf "[-] No .b64 files present"
+                sleep 5
+            fi
+
+        fi
+
+        #elif [[ $is_active_session -eq 0 && $is_file_share_mounted -eq 1 ]]
+        #    then 
+        #        printf "Active session in progress"
+        #        sleep 5
+
+    # If user is not sshed and active session flag is present
+    elif [[ $is_user_sshed -eq 1 && $is_active_session -eq 0 ]]
+        then
+            resetVMSession
     else
-        continue
+        if [[ is_idle -eq 1 ]]
+            then
+            sleep 5
+            printf "."
+        else
+            is_idle=1
+            printf "[+] Idling...\n"
+        fi
     fi
 done
